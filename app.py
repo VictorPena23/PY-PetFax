65365359492ce45b2bbc6c6cbc01123957572a2b
VictorPena23 <halo_rider200219@yahoo.com>
2023-07-05 18:20:11 -0700

    adding imports

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-# config                    
-from flask import Flask
-app = Flask(__name__)
-
-# index route
-@app.route('/')
-def index(): 
-    return 'Hello, this is PetFax!'
+from petfax import create_app
+app = create_app()
